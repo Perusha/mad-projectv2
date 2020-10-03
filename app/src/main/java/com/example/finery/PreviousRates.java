da484e632df5a7808d10bda6acc935e44f190f73
perusha <perushawanniarachchi22@gmail.com>
2020-10-03 19:02:35 +0530

    nithya commit

---
+++ app/src/main/java/com/example/finery/PreviousRates.java
@@ -0,0 +1,14 @@
+package com.example.finery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PreviousRates extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_rates);
+    }
+}
